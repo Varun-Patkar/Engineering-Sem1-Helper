ecbad2bf38241b4d9fc36fe4929f19dd54ed29bb
Varun-Patkar <50433487+Varun-Patkar@users.noreply.github.com>
2020-03-01 21:49:15 +0530

    Penultimate Commit
    Need to straighten pic and add your aboutus. File Upload is also remianing.  Add MCQs also.

--- Helper/urls.py
+++ Helper/urls.py
@@ -8,5 +8,7 @@ urlpatterns=[
     path('subjects_bee',views.bee,name='subjects_bee'),
     path('subjects_mechanics',views.mechanics,name='subjects_mechanics'),
     path('subjects_physics',views.physics,name='subjects_physics'),
-    path('subjects_chemistry',views.chemistry,name='subjects_chemistry')
+    path('subjects_chemistry',views.chemistry,name='subjects_chemistry'),
+    path('aboutus',views.aboutus,name='aboutus'),
+    path('feedback',views.feedback,name='feedback')
 ]
--- Helper/views.py
+++ Helper/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect
 from .models import MCQ,PDF,Announcement
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+def feedback(request):
+    return render(request,'feed-back.html')
+def aboutus(request):
+    return render(request,'aboutus.html')
 def home(request):
     return render(request,'home1.html',{'announcements':Announcement.objects.all()})
 @login_required
@@ -9,16 +13,16 @@ def mcq(request):
     return render(request,'quiz.html',{'mcqs':MCQ.objects.all()})
 @login_required
 def maths(request):
-    return render(request,'subjects/subjects_maths.html',{'iat1':PDF.objects.filter(subject='MATHS', types='IAT1'),'iat2':PDF.objects.filter(subject='MATHS', types='IAT2'),'uni':PDF.objects.filter(subject='MATHS', types='UNI'),'prelim':PDF.objects.filter(subject='MATHS', types='PRELIM'),'tut':PDF.objects.filter(subject='MATHS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='MATHS', types='TT'),'subject':'MATHS'})
+    return render(request,'subjects/subjects_maths.html',{'iat1':PDF.objects.filter(subject='MATHS', types='IAT1'),'iat2':PDF.objects.filter(subject='MATHS', types='IAT2'),'uni':PDF.objects.filter(subject='MATHS', types='UNI'),'prelim':PDF.objects.filter(subject='MATHS', types='PRELIM'),'tut':PDF.objects.filter(subject='MATHS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='MATHS', types='TT'),'subject':'MATHS','announcements':Announcement.objects.all()})
 @login_required
 def bee(request):
-    return render(request,'subjects/subjects_bee.html',{'iat1':PDF.objects.filter(subject='BEE', types='IAT1'),'iat2':PDF.objects.filter(subject='BEE', types='IAT2'),'uni':PDF.objects.filter(subject='BEE', types='UNI'),'prelim':PDF.objects.filter(subject='BEE', types='PRELIM'),'tut':PDF.objects.filter(subject='BEE', types='TUTORIAL'),'tt':PDF.objects.filter(subject='BEE', types='TT'),'subject':'BEE'})
+    return render(request,'subjects/subjects_bee.html',{'iat1':PDF.objects.filter(subject='BEE', types='IAT1'),'iat2':PDF.objects.filter(subject='BEE', types='IAT2'),'uni':PDF.objects.filter(subject='BEE', types='UNI'),'prelim':PDF.objects.filter(subject='BEE', types='PRELIM'),'tut':PDF.objects.filter(subject='BEE', types='TUTORIAL'),'tt':PDF.objects.filter(subject='BEE', types='TT'),'subject':'BEE','announcements':Announcement.objects.all()})
 @login_required
 def chemistry(request):
-    return render(request,'subjects/subjects_chemistry.html',{'iat1':PDF.objects.filter(subject='CHEMISTRY', types='IAT1'),'iat2':PDF.objects.filter(subject='CHEMISTRY', types='IAT2'),'uni':PDF.objects.filter(subject='CHEMISTRY', types='UNI'),'prelim':PDF.objects.filter(subject='CHEMISTRY', types='PRELIM'),'tut':PDF.objects.filter(subject='CHEMISTYRY', types='TUTORIAL'),'tt':PDF.objects.filter(subject='CHEMISTRY', types='TT'),'subject':'CHEMISTRY'})
+    return render(request,'subjects/subjects_chemistry.html',{'iat1':PDF.objects.filter(subject='CHEMISTRY', types='IAT1'),'iat2':PDF.objects.filter(subject='CHEMISTRY', types='IAT2'),'uni':PDF.objects.filter(subject='CHEMISTRY', types='UNI'),'prelim':PDF.objects.filter(subject='CHEMISTRY', types='PRELIM'),'tut':PDF.objects.filter(subject='CHEMISTYRY', types='TUTORIAL'),'tt':PDF.objects.filter(subject='CHEMISTRY', types='TT'),'subject':'CHEMISTRY','announcements':Announcement.objects.all()})
 @login_required
 def physics(request):
-    return render(request,'subjects/subjects_physics.html',{'iat1':PDF.objects.filter(subject='PHYSICS', types='IAT1'),'iat2':PDF.objects.filter(subject='PHYSICS', types='IAT2'),'uni':PDF.objects.filter(subject='PHYSICS', types='UNI'),'prelim':PDF.objects.filter(subject='PHYSICS', types='PRELIM'),'tut':PDF.objects.filter(subject='PHYSICS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='PHYSICS', types='TT'),'subject':'PHYSICS'})
+    return render(request,'subjects/subjects_physics.html',{'iat1':PDF.objects.filter(subject='PHYSICS', types='IAT1'),'iat2':PDF.objects.filter(subject='PHYSICS', types='IAT2'),'uni':PDF.objects.filter(subject='PHYSICS', types='UNI'),'prelim':PDF.objects.filter(subject='PHYSICS', types='PRELIM'),'tut':PDF.objects.filter(subject='PHYSICS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='PHYSICS', types='TT'),'subject':'PHYSICS','announcements':Announcement.objects.all()})
 @login_required
 def mechanics(request):
-    return render(request,'subjects/subjects_mechanics.html',{'iat1':PDF.objects.filter(subject='MECHANICS', types='IAT1'),'iat2':PDF.objects.filter(subject='MECHANICS', types='IAT2'),'uni':PDF.objects.filter(subject='MECHANICS', types='UNI'),'prelim':PDF.objects.filter(subject='MECHANICS', types='PRELIM'),'tut':PDF.objects.filter(subject='MECHANICS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='MECHANICS', types='TT'),'subject':'MECHANICS'})
+    return render(request,'subjects/subjects_mechanics.html',{'iat1':PDF.objects.filter(subject='MECHANICS', types='IAT1'),'iat2':PDF.objects.filter(subject='MECHANICS', types='IAT2'),'uni':PDF.objects.filter(subject='MECHANICS', types='UNI'),'prelim':PDF.objects.filter(subject='MECHANICS', types='PRELIM'),'tut':PDF.objects.filter(subject='MECHANICS', types='TUTORIAL'),'tt':PDF.objects.filter(subject='MECHANICS', types='TT'),'subject':'MECHANICS','announcements':Announcement.objects.all()})
